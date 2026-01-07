a81b26a19cf3d17e4edd1ab55c251eb0be590411
Nikushka2024 <megrelynika@gmail.com>
2026-01-07 12:10:26 +0300

    flake8

--- config/settings.py
+++ config/settings.py
@@ -23,11 +23,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    # libraries
     'rest_framework',
     'drf_yasg',
     'django_filters',
-
+    # apps
     'tracker',
 ]
 
