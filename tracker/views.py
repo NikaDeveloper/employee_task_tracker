17b9c4b31de534edb8fe6e7b61c47322e01a80aa
Nikushka2024 <megrelynika@gmail.com>
2026-01-11 22:45:00 +0300

    views and urls for employee and task

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Employee, Task
+from .serializers import EmployeeSerializer, TaskSerializer
 
-# Create your views here.
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """ CRUD для сотрудников """
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """ CRUD для задач """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
